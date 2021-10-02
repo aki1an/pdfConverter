dbc2afcad2b0b5374263771b391dfb6f9442b731
akilan <53361563+akil4n@users.noreply.github.com>
2021-10-02 23:25:39 +0530

    Update MainController.java

--- src/main/java/com/akilan/onlineConverter/Controller/MainController.java
+++ src/main/java/com/akilan/onlineConverter/Controller/MainController.java
@@ -4,7 +4,7 @@ import com.akilan.onlineConverter.Model.OutputFile;
 import com.akilan.onlineConverter.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;

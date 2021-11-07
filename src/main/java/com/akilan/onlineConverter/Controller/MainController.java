ab2e7c6ba90b3381b219f26cddf9f4c52911c9aa
akil4n <akilanmaruthachalam5678@gmail.com>
2021-11-07 13:07:38 +0530

    allowed cors to all domains

--- src/main/java/com/akilan/onlineConverter/Controller/MainController.java
+++ src/main/java/com/akilan/onlineConverter/Controller/MainController.java
@@ -1,10 +1,9 @@
 package com.akilan.onlineConverter.Controller;
 
-import com.akilan.onlineConverter.Model.OutputFile;
 import com.akilan.onlineConverter.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +17,14 @@ public class MainController {
     @Autowired
     FileService fileService;
 
-    @PostMapping(value = "/word-to-pdf",produces = "application/zip")
+    @PostMapping(value = "/word-to-pdf", produces = "application/zip")
+    @CrossOrigin
     public ResponseEntity wordFilesToPdfFiles(@RequestParam("file") List<MultipartFile> wordFiles) {
         return ResponseEntity.ok(fileService.getListOfWordAndConvertToListOfPDF(wordFiles));
     }
 
-    @PostMapping(value = "/words-to-pdf-merged",produces = "application/pdf")
+    @PostMapping(value = "/words-to-pdf-merged", produces = "application/pdf")
+    @CrossOrigin
     public ResponseEntity wordFilesToMergedPdf(@RequestParam("file") List<MultipartFile> wordFiles) {
         return ResponseEntity.ok(fileService.getListOfWordsAndConvertToSinglePdf(wordFiles).getOutputFile());
     }
